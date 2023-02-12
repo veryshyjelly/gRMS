038df3b70d587e6115f9095f062695a34dd72391
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2023-02-12 16:10:30 +0530

    cleaned up main.go

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"chat-app/database"
-	"chat-app/modals"
 	"gorm.io/gorm/logger"
 )
 
 func main() {
 	database.Connect(logger.Info)
 
-}
-
-func GetDBSrv() modals.DBSrv {
-	return &modals.DBService{
-		DB: database.ChatDb,
-	}
 }
