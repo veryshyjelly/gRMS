4adffd9108321e6fdd4c3134ea880af09bc43b33
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2023-02-12 16:09:10 +0530

    moved functions to message service

--- controllers/message/ForwardMessage.go
+++ controllers/message/ForwardMessage.go
@@ -1,32 +1,27 @@
 package message
 
-import (
-	"chat-app/modals"
-	"gorm.io/gorm"
-)
-
-// ForwardMessage forwards a message to a chat from another chat
-func ForwardMessage(db *gorm.DB, query *ForwardMessageQuery) (*modals.Message, error) {
-	fr, err := modals.GetMessage(db, query.MessageID, query.FromChatID)
-	if err != nil {
-		return nil, err
-	}
-
-	msg, err := modals.CreateMessage(db, query.ChatID, query.From)
-	if err != nil {
-		return nil, err
-	}
-
-	msg.Text = fr.Text
-	msg.Photo = fr.Photo
-	msg.Audio = fr.Audio
-	msg.Document = fr.Document
-	msg.Video = fr.Video
-	msg.Caption = fr.Caption
-
-	err = msg.Insert(db)
-	return msg, err
-}
+//// ForwardMessage forwards a message to a chat from another chat
+//func ForwardMessage(db *gorm.DB, query *ForwardMessageQuery) (*modals.Message, error) {
+//	fr, err := modals.GetMessage(db, query.MessageID, query.FromChatID)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	msg, err := modals.CreateMessage(db, query.ChatID, query.From)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	msg.Text = fr.Text
+//	msg.Photo = fr.Photo
+//	msg.Audio = fr.Audio
+//	msg.Document = fr.Document
+//	msg.Video = fr.Video
+//	msg.Caption = fr.Caption
+//
+//	err = msg.Insert(db)
+//	return msg, err
+//}
 
 // ForwardMessageQuery is the query format for forwarding message
 type ForwardMessageQuery struct {
